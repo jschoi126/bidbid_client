18aaa7c15208bc752e9601ef9ed695063ea3dd0b
jeon3029 <taejjeon@gmail.com>
2016-06-30 19:27:50 +0900

    top menu button 누르면 넘어가는 이벤트
    TODO : 버튼 하이하이트 해야됨.

--- app/src/main/java/com/aze51/bidbid_client/MainActivity.java
+++ app/src/main/java/com/aze51/bidbid_client/MainActivity.java
@@ -1,30 +1,30 @@
 package com.aze51.bidbid_client;
 
-import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.aze51.bidbid_client.Fragment.BottomMenuFragment;
 import com.aze51.bidbid_client.Fragment.TitleFragment;
-import com.aze51.bidbid_client.Fragment.TopMenuFragment;
-import com.aze51.bidbid_client.ViewPager.ListItemData;
-import com.aze51.bidbid_client.ViewPager.RecyclerViewCustomAdapter;
+
 import com.aze51.bidbid_client.ViewPager.ViewPagerCustomAdapter;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     public MainActivity reference;
+
 
     //Fragment Variable
     BottomMenuFragment bottomMenuFragment;
@@ -36,12 +36,14 @@ public class MainActivity extends AppCompatActivity {
 
     //ViewPager
     ViewPager viewpager;
+    LinearLayout currentLinear;
+    LinearLayout scheduledLinear;
+    LinearLayout approachingLinear;
 
-    //Recycler View
-    ArrayList<ListItemData> itemDatas;
-    RecyclerView recyclerView;
-    RecyclerView.Adapter mAdapter;
-    LinearLayoutManager mLayoutManager;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,32 +51,33 @@ public class MainActivity extends AppCompatActivity {
         reference = this;
         initiate();
         show_current_list();
+
     }
     //Made By Tae Joon 2016 06 27 : 현재 판매중인 목록 프래그먼트로 보여주기.
     public void show_current_list() {
-
-
         fragmentManager.beginTransaction().add(R.id.TitleLayout, titleFragment).commit();
-        fragmentManager.beginTransaction().add(R.id.TopMenuLayout, topMenuFragment).commit();
+
         fragmentManager.beginTransaction().add(R.id.ListLayout,listFragment).commit();
+
+        fragmentManager.beginTransaction().add(R.id.TopMenuLayout, topMenuFragment).commit();
+
         fragmentManager.beginTransaction().add(R.id.BottomLayout, bottomMenuFragment).commit();
     }
     public void show_scheduled_list(){
         fragmentManager.beginTransaction().add(R.id.TitleLayout, titleFragment).commit();
-        fragmentManager.beginTransaction().add(R.id.TopMenuLayout, topMenuFragment).commit();
+
         fragmentManager.beginTransaction().add(R.id.ListLayout,listFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.TopMenuLayout, topMenuFragment).commit();
         fragmentManager.beginTransaction().add(R.id.BottomLayout, bottomMenuFragment).commit();
 
     }
     public void show_approaching_list(){
         fragmentManager.beginTransaction().add(R.id.TitleLayout, titleFragment).commit();
-        fragmentManager.beginTransaction().add(R.id.TopMenuLayout, topMenuFragment).commit();
+
         fragmentManager.beginTransaction().add(R.id.ListLayout,listFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.TopMenuLayout, topMenuFragment).commit();
         fragmentManager.beginTransaction().add(R.id.BottomLayout, bottomMenuFragment).commit();
-
-
     }
-
     //Made By Tae Joon 2016 06 27 : 초기화
     private void initiate() {
         bottomMenuFragment = new BottomMenuFragment();
@@ -82,10 +85,14 @@ public class MainActivity extends AppCompatActivity {
         topMenuFragment = new TopMenuFragment();
         fragmentManager = getSupportFragmentManager();
         titleFragment = new TitleFragment();
+
+        currentLinear = (LinearLayout)findViewById(R.id.linear_current);
+        scheduledLinear = (LinearLayout)findViewById(R.id.linear_scheduled);
+        approachingLinear = (LinearLayout)findViewById(R.id.linear_approaching);
+
+
     }
     public class ListFragment extends Fragment { //view pager 사용해서 리사이클러 뷰 띄움
-
-        Context ctx;
         public ListFragment(){
             //생성자
         }
@@ -96,6 +103,41 @@ public class MainActivity extends AppCompatActivity {
             viewpager = (ViewPager) rootViewBasic.findViewById(R.id.viewPager);
             //ctx = getActivity().getApplicationContext();
             viewpager.setAdapter(new ViewPagerCustomAdapter(reference));//Main Activity 의 this 를 보내야함.
+
+            btn1 = (Button) rootViewBasic.findViewById(R.id.current_btn);
+            btn2 = (Button) rootViewBasic.findViewById(R.id.scheduled_btn);
+            btn3 = (Button) rootViewBasic.findViewById(R.id.approaching_btn);
+            return rootViewBasic;
+        }
+    }
+    public class TopMenuFragment extends Fragment {
+        Button btn1;
+        Button btn2;
+        Button btn3;
+        View rootViewBasic;
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            rootViewBasic = inflater.inflate(R.layout.top_menu_fragment,container,false);
+
+            btn1 = (Button) rootViewBasic.findViewById(R.id.current_btn);
+            btn2 = (Button) rootViewBasic.findViewById(R.id.scheduled_btn);
+            btn3 = (Button) rootViewBasic.findViewById(R.id.approaching_btn);
+            btn1.setOnClickListener(new Button.OnClickListener(){
+                public void onClick(View V){
+                    viewpager.setCurrentItem(0);
+                }
+            });
+            btn2.setOnClickListener(new Button.OnClickListener(){
+                public void onClick(View V){
+                    viewpager.setCurrentItem(1);
+                }
+            });
+            btn3.setOnClickListener(new Button.OnClickListener(){
+                public void onClick(View V){
+                    viewpager.setCurrentItem(2);
+                }
+            });
             return rootViewBasic;
         }
     }
--- app/src/main/java/com/aze51/bidbid_client/ViewPager/ViewPagerCustomAdapter.java
+++ app/src/main/java/com/aze51/bidbid_client/ViewPager/ViewPagerCustomAdapter.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.aze51.bidbid_client.R;
 
@@ -22,15 +24,17 @@ public class ViewPagerCustomAdapter extends PagerAdapter {
     RecyclerView recyclerView;
     RecyclerView.Adapter mAdapter;
     LinearLayoutManager mLayoutManager;
+
     public ViewPagerCustomAdapter(Context context) {
         mContext = context;
     }
+
     @Override
-    public Object instantiateItem(ViewGroup collection, int position) {
+    public Object instantiateItem(ViewGroup collection, final int position) {
+
         ModelObject modelObject = ModelObject.values()[position];
         LayoutInflater inflater = LayoutInflater.from(mContext);
         ViewGroup layout = (ViewGroup) inflater.inflate(modelObject.getLayoutResId(), collection, false);
-
         collection.addView(layout);
 
         if(position == 0) {//첫 번째 페이지 일 경우
@@ -45,7 +49,35 @@ public class ViewPagerCustomAdapter extends PagerAdapter {
             itemDatas = new ArrayList<ListItemData>();
             mAdapter = new RecyclerViewCustomAdapter(itemDatas);
             recyclerView.setAdapter(mAdapter);
-            itemDatas.add(new ListItemData(R.mipmap.b,"이름","가격","3:57 남음"));
+            itemDatas.add(new ListItemData(R.mipmap.b,"이름111","가격1111","3:57 남음"));
+        }
+        else if(position == 1){
+            recyclerView = (RecyclerView) collection.findViewById(R.id.recyclerView_scheduled);
+            //아이템이 일정할 경우 고정
+            recyclerView.setHasFixedSize(true);
+            //LayoutManager 초기화
+            mLayoutManager = new LinearLayoutManager(mContext.getApplicationContext());//Mainactivity 의 this
+            mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(mLayoutManager);
+            //adapter 설정
+            itemDatas = new ArrayList<ListItemData>();
+            mAdapter = new RecyclerViewCustomAdapter(itemDatas);
+            recyclerView.setAdapter(mAdapter);
+            itemDatas.add(new ListItemData(R.mipmap.b,"이름222","가격2222","3:57 남음"));
+        }
+        else if(position == 2){
+            recyclerView = (RecyclerView) collection.findViewById(R.id.recyclerView_approaching);
+            //아이템이 일정할 경우 고정
+            recyclerView.setHasFixedSize(true);
+            //LayoutManager 초기화
+            mLayoutManager = new LinearLayoutManager(mContext.getApplicationContext());//Mainactivity 의 this
+            mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(mLayoutManager);
+            //adapter 설정
+            itemDatas = new ArrayList<ListItemData>();
+            mAdapter = new RecyclerViewCustomAdapter(itemDatas);
+            recyclerView.setAdapter(mAdapter);
+            itemDatas.add(new ListItemData(R.mipmap.b,"이름333","가격3333","3:57 남음"));
         }
         return layout;
     }
