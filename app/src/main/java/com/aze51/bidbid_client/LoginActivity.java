47bc8b44e10dca0cce6581dd63377612564c7fe0
jeon3029 <taejjeon@gmail.com>
2016-06-30 22:57:33 +0900

    test

--- app/src/main/java/com/aze51/bidbid_client/LoginActivity.java
+++ app/src/main/java/com/aze51/bidbid_client/LoginActivity.java
@@ -3,16 +3,42 @@ package com.aze51.bidbid_client;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
     Button loginButton;
     Button joinButton;
+
+    //facebook
+    LoginButton facebook_loginButton;
+    private CallbackManager callbackManager = null;
+    private AccessTokenTracker accessTokenTracker = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        //setContentView(R.layout.activity_login);
+    }
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_login, container, false);
+
         loginButton = (Button)findViewById(R.id.LoginButton);
         joinButton =(Button) findViewById(R.id.join_button);
         loginButton.setOnClickListener(new View.OnClickListener(){
@@ -31,6 +57,30 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
-    }
 
+
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        // If using in a fragment
+        loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }
 }
