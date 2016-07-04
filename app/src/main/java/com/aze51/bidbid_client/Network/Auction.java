1a55399c38782911686e960dab5b559c1db2eb33
Leekyungho <leekh034@gmail.com>
2016-07-04 14:00:03 +0900

    add Auction class

---
+++ app/src/main/java/com/aze51/bidbid_client/Network/Auction.java
@@ -0,0 +1,10 @@
+package com.aze51.bidbid_client.Network;
+
+/**
+ * Created by Leekh on 2016-07-03.
+ */
+public class Auction {
+    public int register_id;
+    public String user_id;
+    public int deal_price;
+}
