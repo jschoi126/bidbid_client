8fa7e32b36492da97ad23bd29cf3c76ac5ae27ff
unknown <>
2016-07-05 17:05:02 +0900

    back버튼 눌렀을 때 뒤로가기 완성!!

--- app/src/main/java/com/aze51/bidbid_client/SettingActivity.java
+++ app/src/main/java/com/aze51/bidbid_client/SettingActivity.java
@@ -2,11 +2,25 @@ package com.aze51.bidbid_client;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SettingActivity extends AppCompatActivity {
+
+    ImageView logo_image;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        logo_image = (ImageView)findViewById(R.id.logo_image);
+        logo_image.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
