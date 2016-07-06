9ffa7bd157d19d1895cbdac1362d54339bdaf2d6
최준성 <cjs@estmob.com>
2016-07-06 23:24:53 +0900

    Add AppIntro

---
+++ app/src/main/java/com/aze51/bidbid_client/AppIntro/IntroFragment.java
@@ -0,0 +1,22 @@
+package com.aze51.bidbid_client.AppIntro;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aze51.bidbid_client.R;
+
+/**
+ * Created by ChoiJunsung on 2016. 7. 6..
+ */
+public class IntroFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.app_intro_1, container, false);
+        return view;
+    }
+}
